b2eb41373a7a7eebcb517f1ddc05cfe1c441afa6
Serkan Kumru <serkankumru1997@gmail.com>
2019-11-25 09:52:42 +0300

    Add files via upload

---
+++ Domain/NewsContext.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public partial class NewsApiDBEntities
+    {
+
+        private static NewsApiDBEntities _context;
+        public static NewsApiDBEntities CreateInstanceSingleton()
+        {
+            if (_context == null)
+                _context = new NewsApiDBEntities();
+            return _context;
+        }
+    }
+}
